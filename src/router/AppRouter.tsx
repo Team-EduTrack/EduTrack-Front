97b41d7deb4fea06ba0051676981ddfcd1b5d37f
Minkyung Kim <alsrud9138@gmail.com>
2025-11-25 13:01:26 +0900

    feat: principal dashboard UI, student page rename, and router updates

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "../pages/auth/Login";
 import Signup from "../pages/auth/Signup";
-import StudentDashboard from "../pages/student/Dashboard";
+import StudentDashboard from "../pages/student/StudentDashboard";
 import UserManagement from "../pages/admin/UserManagement";
-import Layout from "../components/common/layout/Layout";
+import PrincipalDashBoard from "../pages/principal/principalDashboard";
+import PrincipalUserManagement from "../pages/principal/PrincipalUserManagement";
+import LectureManagement from "../pages/principal/LectureManagement";
 
 export default function AppRouter() {
   return (
@@ -11,12 +13,13 @@ export default function AppRouter() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
-      <Route element={<Layout />}>
-        <Route path="/student/dashboard" element={<StudentDashboard />} />
-        {/* <Route path="/teacher/dashboard" element={<TeacherDashboard />} /> */}
-      </Route>
+      <Route path="/student/dashboard" element={<StudentDashboard />} />
 
       <Route path="/admin/users" element={<UserManagement />} />
+
+      <Route path="/principal/dashboard" element={<PrincipalDashBoard />} />
+      <Route path="/principal/users" element={<PrincipalUserManagement />} />
+      <Route path="/principal/lectures" element={<LectureManagement />} />
 
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="*" element={<h1>404 Not Found</h1>} />
